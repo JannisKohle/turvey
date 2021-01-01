74c48431132636d2e8ef07b649e182fbf39f4200
JannisKohle <Jannis.Kohle@icloud.com>
2021-01-01 20:31:26 +0100

    really tiny changes

--- server/routes/surveysRouter.js
+++ server/routes/surveysRouter.js
@@ -3,4 +3,16 @@ const router = express.Router();
 
 router.use(express.json());
 
+router.get("/:surveyId", (req, res) => {
+  res.send();
+});
+
+router.post("/:surveyId", (req, res) => {
+    res.send();
+});
+
+router.delete("/:survey", (req, res) => {
+    res.send();
+});
+
 module.exports = router;
